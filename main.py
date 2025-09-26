08c0ddd786666eb034293ceae8f6f9e4a74755ed
MathiasBroeckx <r1042594@student.thomasmore.be>
2025-09-26 13:12:19 +0200

    Split center table in two

--- main.py
+++ main.py
@@ -113,8 +113,11 @@ class HauntedKitchen:
 
         self.colliding_objects = [
             CollisionObject(
-        300, 250, constants.SCREEN_WIDTH - 600, 250,
-        texture_path="wood_tile.png"),
+            300, 250, constants.SCREEN_WIDTH - 1400, 250,
+            texture_path="wood_tile.png"),
+            CollisionObject(
+            constants.SCREEN_WIDTH - 830, 250, constants.SCREEN_WIDTH - 1400, 250,
+            texture_path="wood_tile.png"),
             CollisionObject(0, constants.SCREEN_HEIGHT -
                             self.safe_zone.height - 5, 100, 10),
             CollisionObject(200, constants.SCREEN_HEIGHT -
