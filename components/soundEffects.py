c07d496b5eea80c6dc9bfc6f53c0cf7199751446
uijdensryano <uijdensryano@gmail.com>
2025-09-26 11:02:48 +0200

    whisper toegoevoegd

--- components/soundEffects.py
+++ components/soundEffects.py
@@ -2,6 +2,7 @@ import pygame
 
 def play_soundeffect(sound):
     soundeffect_dict = {"step": "Assets/footsteps-male-362053.mp3",
-                        "scream": "Assets/male-death-scream-horror-352706.mp3"}
+                        "scream": "Assets/male-death-scream-horror-352706.mp3",
+                        "whisper": "Assets/creepy-ghost-whisper-410564.mp3"}
 
     pygame.mixer.Sound(soundeffect_dict[sound]).play()
